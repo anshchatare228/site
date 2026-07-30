ff454fae5f3f526b858278becc480991c3ad96e3
Ansh Chatare <ansh.chatare.228@gmail.com>
2026-07-30 20:13:31 +0530

    React compatibility for pages update

--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,7 @@ export default defineConfig({
   plugins: [
     tailwindcss(),
     react(),
-    babel({ presets: [reactCompilerPreset()] })
+    babel({ presets: [reactCompilerPreset()] }),
   ],
+  base: '/site/'
 })
